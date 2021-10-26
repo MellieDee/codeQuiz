5716bf42886bd6fad817d101074c47784528abea
Mellie Dee <89175650+MellieDee@users.noreply.github.com>
2021-10-26 01:04:06 -0700

    build initial HTML & CSS structure added footer elements & style

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+var countDownTimerEl = document.getElementById('timer');
+
+
+//  ***   COUNTDOWN TIMER function ***
+
+//click start button
+//start buttoon intiates countdown
+//and first question (5-10 questions).( from 1 min try to st - 1 min= 60000)
+//the countdown is displayed, continously decreased util 0
+//when reaches 0, timer stops. game is over.
+//time is deducted if a question is wrong
+
+// high scores: -- see taskinator body for timer pacement
+//create a list like the taskinator - 
+// how manke last question disappear and get replaced with high score menu?
+
+
+// use JS to change btn color for hs & questions
+
+//get the HTML/DOM El
+var countDownTimerEl = document.getElementById('timer');
+
